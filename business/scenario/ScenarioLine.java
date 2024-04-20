0af2be04e11e4e932f7b1321775924242e465b53
Simon <simon.strykers@gmail.com>
2024-04-20 12:31:13 +0200

    déplacement du Pattern dans la fonction de création de ScenarioLine, n'étant utilisé que dedans

--- business/scenario/ScenarioLine.java
+++ business/scenario/ScenarioLine.java
@@ -9,7 +9,6 @@ public class ScenarioLine {
     public String type;
     public String name;
     public Coordinates coordinates;
-    static private final Pattern lineRegex = Pattern.compile("^(?<type>Baloon|JetPlane|Helicopter)\\s*(?<name>\\w*)\\s*(?<longitude>\\d*)\\s*(?<latitude>\\d*)\\s*(?<height>\\d*)$");
 
     private ScenarioLine(String type, String name, Coordinates coordinates) {
       this.type = type;
@@ -18,10 +17,13 @@ public class ScenarioLine {
     }
 
     public static ScenarioLine parseLine(String line) throws Exception {
+      Pattern lineRegex = Pattern.compile("^(?<type>Baloon|JetPlane|Helicopter)\\s*(?<name>\\w*)\\s*(?<longitude>\\d*)\\s*(?<latitude>\\d*)\\s*(?<height>\\d*)$");
       Matcher matcher = lineRegex.matcher(line);
+
       if (!matcher.find()) {
         throw new Exception("ScenarioLine: invalid line");
       }
+
       String name = matcher.group("name");
       String type = matcher.group("type").toUpperCase();
       int longitude = Integer.parseInt(matcher.group("longitude"));
