849af490d6f5c55cb478f98bc135ac1fa7cb551c
Simon <simon.strykers@gmail.com>
2024-04-20 12:02:32 +0200

    Utilisation d'un try-with-ressource pour le BufferedReader (aussi, désolé, j'avais pas changé les paramètres concernant l'indentation. J'ai remis pour que ça colle à ta pratique ici)

--- business/scenario/ScenarioInterpreter.java
+++ business/scenario/ScenarioInterpreter.java
@@ -15,82 +15,77 @@ import business.flyable.Flyable;
 import business.tower.WeatherTower;
 
 public class ScenarioInterpreter {
-    private final Path filePath;
-    private int iterations;
-    private final List<ScenarioLine> lines;
+  private final Path filePath;
+  private int iterations;
+  private final List<ScenarioLine> lines;
 
-    static class ScenarioLine {
-        public String type;
-        public String name;
-        public Coordinates coordinates;
-        static private final Pattern lineRegex = Pattern.compile("^(?<type>Baloon|JetPlane|Helicopter)\\s*(?<name>\\w*)\\s*(?<longitude>\\d*)\\s*(?<latitude>\\d*)\\s*(?<height>\\d*)$");
+  static class ScenarioLine {
+    public String type;
+    public String name;
+    public Coordinates coordinates;
+    static private final Pattern lineRegex = Pattern.compile("^(?<type>Baloon|JetPlane|Helicopter)\\s*(?<name>\\w*)\\s*(?<longitude>\\d*)\\s*(?<latitude>\\d*)\\s*(?<height>\\d*)$");
 
-        ScenarioLine(String type, String name, Coordinates coordinates) {
-            this.type = type;
-            this.name = name;
-            this.coordinates = coordinates;
-        }
-
-        public static ScenarioLine parseLine(String line) throws Exception {
-            Matcher matcher = lineRegex.matcher(line);
-            if (!matcher.find()) {
-                throw new Exception("ScenarioLine: invalid line");
-            }
-            String name = matcher.group("name");
-            String type = matcher.group("type").toUpperCase();
-            int longitude = Integer.parseInt(matcher.group("longitude"));
-            int latitude = Integer.parseInt(matcher.group("latitude"));
-            int height = Integer.parseInt(matcher.group("height"));
-
-            return new ScenarioLine(type, name, new Coordinates(longitude, latitude, height));
-        }
+    ScenarioLine(String type, String name, Coordinates coordinates) {
+      this.type = type;
+      this.name = name;
+      this.coordinates = coordinates;
     }
 
-    public ScenarioInterpreter(Path filePath) {
-        verifValidFile(filePath);
+    public static ScenarioLine parseLine(String line) throws Exception {
+      Matcher matcher = lineRegex.matcher(line);
+      if (!matcher.find()) {
+        throw new Exception("ScenarioLine: invalid line");
+      }
+      String name = matcher.group("name");
+      String type = matcher.group("type").toUpperCase();
+      int longitude = Integer.parseInt(matcher.group("longitude"));
+      int latitude = Integer.parseInt(matcher.group("latitude"));
+      int height = Integer.parseInt(matcher.group("height"));
 
-        this.filePath = filePath;
-        this.iterations = 0;
-        this.lines = new LinkedList<>();
+      return new ScenarioLine(type, name, new Coordinates(longitude, latitude, height));
+    }
+  }
+
+  public ScenarioInterpreter(Path filePath) {
+    verifValidFile(filePath);
+
+    this.filePath = filePath;
+    this.iterations = 0;
+    this.lines = new LinkedList<>();
+  }
+
+  public void read() throws Exception {
+    try (BufferedReader reader = new BufferedReader(new FileReader(this.filePath.toString()))) {
+
+      String line;
+      while ((line = reader.readLine()) != null) {
+        this.lines.add(ScenarioLine.parseLine(line));
+      }
+    }
+  }
+
+  public void execute() throws Exception {
+    WeatherTower weatherTower = new WeatherTower();
+    AircraftFactory aircraftFactory = AircraftFactory.getInstance();
+
+    for (ScenarioLine sl : this.lines) {
+      Flyable flyable = aircraftFactory.newAircraft(sl.type, sl.name, sl.coordinates);
+
+      weatherTower.register(flyable);
     }
 
-    public void read() throws Exception {
-        BufferedReader reader = new BufferedReader(new FileReader(this.filePath.toString()));
+    for (int i = 0; i < this.iterations; i++) {
+      weatherTower.changeWeather();
+    }
+  }
 
-        // Parse iterations
-        String line = reader.readLine();
-
-        this.iterations = Integer.parseInt(line);
-
-        while ((line = reader.readLine()) != null) {
-            this.lines.add(ScenarioLine.parseLine(line));
-        }
-
-        reader.close();
+  private void verifValidFile(Path filePath) {
+    if (Files.notExists(filePath)) {
+      throw new IllegalArgumentException("ScenarioInterpreter: invalid scenario file: file does not exist");
     }
 
-    public void execute() throws Exception {
-        WeatherTower weatherTower = new WeatherTower();
-        AircraftFactory aircraftFactory = AircraftFactory.getInstance();
-
-        for (ScenarioLine sl : this.lines) {
-            Flyable flyable = aircraftFactory.newAircraft(sl.type, sl.name, sl.coordinates);
-
-            weatherTower.register(flyable);
-        }
-
-        for (int i = 0 ; i < this.iterations ; i++) {
-            weatherTower.changeWeather();
-        }
+    if (filePath.toFile().length() == 0) {
+      throw new IllegalArgumentException("ScenarioInterpreter: invalid scenario file: empty file");
     }
-
-    private void verifValidFile(Path filePath) {
-        if (Files.notExists(filePath)) {
-            throw new IllegalArgumentException("ScenarioInterpreter: invalid scenario file: file does not exist");
-        }
-
-        if (filePath.toFile().length() == 0) {
-            throw new IllegalArgumentException("ScenarioInterpreter: invalid scenario file: empty file");
-        }
-    }
+  }
 }
